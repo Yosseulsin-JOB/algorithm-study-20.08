a85bec3960d686a3b9fbe763b32f8824bc04f80b
sotaneum <gnyontu39@gmail.com>
2020-09-08 23:09:51 +0900

    2차 이동건

--- 2차/이동건_가운데-글자-가져오기.py
+++ 2차/이동건_가운데-글자-가져오기.py
@@ -1,6 +1,4 @@
 # 22:03 ~ 22:09
 def solution(s):
     middle = int(len(s)/2)
-    if len(s)%2 == 1:
-        return s[middle]
-    return s[middle - 1: middle + 1]
+    return s[middle - 1: middle + 1] if len(s)%2 == 0 else s[middle]
